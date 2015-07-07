920e5ce55e4bd47848bef42081ce2246d63e9060
paul <paul.zakharov@gmail.com>
2015-07-07 18:53:43 +0200

    test qt_test

--- qt_test.py
+++ qt_test.py
@@ -13,29 +13,65 @@ last edited: October 2011
 """
 
 import sys
-from PyQt4 import QtGui
+import csv
+from PyQt4 import QtGui, QtCore
 
 class Example(QtGui.QWidget):
     
     def __init__(self):
         super(Example, self).__init__()
         
-        self.initUI()
-        
+        self.initUI()        
         
     def initUI(self):
         
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QtGui.QIcon('web.png'))        
+#         self.setGeometry(300, 300, 250, 150)
+#         self.setWindowTitle('Icon')
+#         self.setWindowIcon(QtGui.QIcon('web.png'))        
     
-        self.show()
+        self.model = QtGui.QStandardItemModel(self)
+ 
+        self.tableView = QtGui.QTableView(self)
+        self.tableView.setModel(self.model)
+        self.tableView.horizontalHeader().setStretchLastSection(True)
+ 
+        self.pushButtonLoad = QtGui.QPushButton(self)
+        self.pushButtonLoad.setText("Load Csv File!")
+        self.pushButtonLoad.clicked.connect(self.on_pushButtonLoad_clicked)
+ 
+        self.pushButtonWrite = QtGui.QPushButton(self)
+        self.pushButtonWrite.setText("Write Csv File!")
+        self.pushButtonWrite.clicked.connect(self.on_pushButtonWrite_clicked)
+ 
+        self.layoutVertical = QtGui.QVBoxLayout(self)
+        self.layoutVertical.addWidget(self.tableView)
+        self.layoutVertical.addWidget(self.pushButtonLoad)
+        self.layoutVertical.addWidget(self.pushButtonWrite)
+
+    def loadCsv(self, fileName):
+        with open(fileName, "rb") as fileInput:
+            for row in csv.reader(fileInput):    
+                items = [
+                    QtGui.QStandardItem(field)
+                    for field in row
+                ]
+                self.model.appendRow(items)
+    
+    @QtCore.pyqtSlot()
+    def on_pushButtonWrite_clicked(self):
+        self.writeCsv(self.fileName)
+
+    @QtCore.pyqtSlot()
+    def on_pushButtonLoad_clicked(self):
+        #self.loadCsv(self.fileName)
+        self.loadCsv("/home/paul/Inet_Perso/0515/NFL/coaches__coaches.csv")
         
         
 def main():
     
     app = QtGui.QApplication(sys.argv)
     ex = Example()
+    ex.show()
     sys.exit(app.exec_())
 
 
